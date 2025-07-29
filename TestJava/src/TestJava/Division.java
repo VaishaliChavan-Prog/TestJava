03185319dfb29c3478db3fd33d90721f94473e69
VaishaliChavan-Prog <vaishalichavan4423@gmail.com>
2025-07-29 16:15:50 +0530

    Push division.java file

---
+++ TestJava/src/TestJava/Division.java
@@ -0,0 +1,13 @@
+package TestJava;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int a=1200,b=30,c;
+		c=a/b;
+	    System.out.print("The division of a & b is : "+c);
+	    
+
+	}
+
+}
