4e505878bfa467920c1e2b8530712ef30f980aa9
VaishaliChavan-Prog <vaishalichavan4423@gmail.com>
2025-07-29 16:14:02 +0530

    added addition.java file

---
+++ TestJava/src/TestJava/Addition.java
@@ -0,0 +1,12 @@
+package TestJava;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int b=900, d=400,addition;
+		addition=b+d;
+		System.out.print("Addition of b & d is :" +addition);
+
+	}
+
+}
