62f3909384faeb2172eee89e214bb22f8d0f7fd2
VaishaliChavan-Prog <vaishalichavan4423@gmail.com>
2025-07-29 16:15:21 +0530

    Push multiplication.java file

---
+++ TestJava/src/TestJava/Multiplication.java
@@ -0,0 +1,11 @@
+package TestJava;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		int n=30,p=20,m;
+        m=n*p;
+        System.out.print("Mulitiplication of n & p is : "+m);
+	}
+
+}
